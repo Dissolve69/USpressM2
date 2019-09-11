f403c015a424b2903c3cb1dfce67287d7f840eef
Dissolve69 <50832448+Dissolve69@users.noreply.github.com>
2019-09-11 22:07:00 +0900

    Update wordscount.py

--- wordscount.py
+++ wordscount.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import operator
 
+## 소스 코드는 압축적이라 짧아요. 이 프로그램 실행 했을때 결과 화면도 함께 보내드리께요. 
 
 def count(request):
     fulltext = request.GET['fulltext']
